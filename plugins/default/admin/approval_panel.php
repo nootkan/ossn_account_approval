aa4e08462b6d55f2b209e8e878ae93ce845b5b16
nootkan <info@sportsmanfishing.com>
2026-05-31 12:12:19 -0700

    Add files via upload

--- plugins/default/admin/approval_panel.php
+++ plugins/default/admin/approval_panel.php
@@ -1,1 +1,6 @@
-
+<?php
+/**
+ * Open Source Social Network - AccountApproval
+ * Legacy view file (kept for compatibility, main view is account_approval/admin/manage.php)
+ */
+// This file intentionally left minimal - the active view is account_approval/admin/manage.php
